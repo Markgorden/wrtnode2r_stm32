98c60076a10a36185c27713d750d90a6af985577
SchumyHao <bob-hjl@126.com>
2016-04-10 11:08:14 +0800

    fix scons compile problem
    Signed-off-by: SchumyHao <bob-hjl@126.com>

--- rtconfig.py
+++ rtconfig.py
@@ -14,7 +14,7 @@ EXEC_PATH = '/Users/schumy/arm-gcc/bin'
 if os.getenv('RTT_EXEC_PATH'):
     EXEC_PATH = os.getenv('RTT_EXEC_PATH')
 
-BUILD = 'debug'
+BUILD = ''
 STM32_TYPE = 'STM32F10X_MD'
 R_VERSION = 'V0.9'
 
@@ -44,5 +44,5 @@ if BUILD == 'debug':
 else:
     CFLAGS += ' -Og'
 
-    POST_ACTION = OBJCPY + ' -O binary $TARGET WRTnode_2r_STM32_rtthread_' + R_VERSION + '.bin\n' + SIZE + ' $TARGET \n'
+POST_ACTION = OBJCPY + ' -O binary $TARGET WRTnode_2r_STM32_rtthread_' + R_VERSION + '.bin\n' + SIZE + ' $TARGET \n'
 
